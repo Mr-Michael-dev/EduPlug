b5c76970b07c1cb22276c875b1ac8449348e51e5
unknown <tobiadesiyan007@gmail.com>
2024-09-12 23:03:15 +0100

    changes

--- frontend/react/src/Login/Login.js
+++ frontend/react/src/Login/Login.js
@@ -6,8 +6,12 @@ function Login() {
     <React.Fragment>
       <div className="App-body">
         <form>
-          <input type="email" name="Email Address" placeholder="Email Address">           </input>
-          <input type="password" name="password" placeholder="Password"></input>          <p>By signing up, you are agreeing to our privacy policy, terms of use and code of conduct.</p>
+          <input type="email" name="Email Address" placeholder="Email Address"></input>
+
+          <input type="password" name="password" placeholder="Password"></input>
+
+	  <p>By signing up, you are agreeing to our privacy policy, terms of use and code of conduct.</p>
+
           <input type="Login" value="Login"></input>
         </form>
       </div>
